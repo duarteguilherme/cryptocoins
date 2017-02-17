c08471cc3f93ee0a3843875633143bcda76345b9
gjardimduarte@gmail.com <dunha@dunha.com>
2017-02-17 18:44:22 -0200

    first commit

---
+++ R/utils.R
@@ -0,0 +1,6 @@
+# Useful and general functions for the package 'cryptocoins'
+
+`%p%` <- function(e1,e2) {
+  # This pipe unites two characters. It works similarly to '+' in python. Example: "United " %p% "States".
+  return(paste0(e1,e2))
+}
