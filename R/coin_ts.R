92dcb18666375d2d6388153e7d4204b96f7e9f05
Guilherme <gjardimduarte@gmail.com>
2017-02-17 22:28:03 -0200

    Fixed function

--- R/coin_ts.R
+++ R/coin_ts.R
@@ -1,10 +1,14 @@
-# This function fetches time series data for a certain cryptocoin
-#
-# It employs POLONIEX API (https://poloniex.com/support/api/)
+#' This function fetches time series data for a certain cryptocoin
+#'
+#' It employs POLONIEX API (https://poloniex.com/support/api/)
 
-coin_ts <- function(from, to="USD", start=,end=Sys.Date(), period=300) {
+coin_ts <- function(from="USDT", to="BTC", start=ymd("2001-01-01"),end=Sys.Date(), period=86400) {
+  end <- as.numeric(as.POSIXct(end))
+  start <- as.numeric(as.POSIXct(start))
   link <- "https://poloniex.com/public?command=returnChartData&currencyPair=" %p%
-    "BTC" %p% "_" %p% "XMR" %p%
+    from %p% "_" %p% to %p%
     "&start=" %p% start %p% "&end=" %p% end %p% "&period=" %p% period
+  data_ts <- fromJSON(link)
+  data_ts <- mutate(data_ts, date=as.Date(as.POSIXct(date, origin="1970-01-01")))
 
 }
