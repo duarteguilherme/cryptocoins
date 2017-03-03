5e3ddd3b22ba136c08d5b165d8037029959dce3a
guilherme <gjardimduarte@gmail.com>
2017-03-03 13:39:08 -0300

    fix namespace

--- R/coin_ts.R
+++ R/coin_ts.R
@@ -15,7 +15,7 @@
 #' btc_data <- coin_ts(from="BTC", to="USDT", start="2001-01-01", end="2017-02-18")
 #' @export
 
-coin_ts <- function(from="BTC", to="USDT", start="2001-01-01",end="9999-01-01", period=86400) {
+coin_ts <- function(from="USDT", to="ETH", start="2001-01-01",end="9999-01-01", period=86400) {
   end <- as.numeric(as.POSIXct(ymd(end)))
   start <- as.numeric(as.POSIXct(ymd(start)))
   link <- "https://poloniex.com/public?command=returnChartData&currencyPair=" %p%
